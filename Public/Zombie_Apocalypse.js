7c1ef77fbf8720fbbd380022122fdbe0aa94f854
keithwv <48747720+keithwv@users.noreply.github.com>
2021-10-25 00:28:59 -0600

    Added radio event listener so that go to a different scene depending on which radio button is selected

--- Public/Zombie_Apocalypse.js
+++ Public/Zombie_Apocalypse.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     let button = document.querySelector('#Next-button')
     let content = document.querySelector('#content')
-    button.addEventListener('click', function() {
-        content.innerHTML = "<h1> New Scene! <h1>"
+    let choice1 = document.querySelector('#radio1')
+    let choice2 = document.querySelector('#radio2')
+    
+    choice1.addEventListener("click", function() {
+        button.addEventListener('click', function() {
+            content.innerHTML = "<h1> New Scene! <h1>"
+        })
     });
+    choice2.addEventListener("click", function() {
+        button.addEventListener('click', function() {
+            content.innerHTML = "<h1> Die! <h1>"
+    })
+})
 });
